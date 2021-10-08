9a22fce798a8cd76fb5e2924ae7df5d8cd68aa6b
ELIABE KLIPPEL <78990377+Eliabek07@users.noreply.github.com>
2021-10-08 16:23:07 +0000

    Complet

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -21,3 +21,6 @@ public class ContaBancaria{
     str+="Saldo: " + this.saldo +"\tEstado: "+ this.estado +"\n";
 
     return str;
+
+  }
+}
--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
     
   Aluno aluno0 = new Aluno("Rogerio", "ADM", 333.55, 2);
   Aluno aluno1 = new Aluno("Maria", "ADS", 290.55, 1);
-  Aluno aluno2 = new Aluno("Rogerio", "LOG", 500.55, 2); 
+  Aluno aluno2 = new Aluno("Roberto Carlos Assis", "LOG", 500.55, 2); 
     
     Aluno [] vetAluno = new Aluno [3];
 
@@ -15,8 +15,22 @@ class Main {
     for (int i = 0; i < vetAluno.length; i++)
     System.out.println(vetAluno[i]);
 
-  ContaBancaria pessoa1 = new ContaBancaria("Eliabe", "15", 522, false);
+  ContaBancaria cliente1 = new ContaBancaria("Eliabe", "15", 522, false);
+  ContaBancaria cliente2 = new ContaBancaria("Ana", "2765", 25.000, true);
+  ContaBancaria cliente3 = new ContaBancaria("Silas", "29", 35.500, false);
     
+
+    ContaBancaria [] vetCliente = new ContaBancaria [3];
+
+    vetCliente[0] = cliente1;
+    vetCliente[1] = cliente2;
+    vetCliente[2] = cliente3;
+
+
+    for (int e = 0; e < vetCliente.length; e++)
+    System.out.println(vetCliente[e]);
+
+
 
   }
 }
